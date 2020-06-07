200cef714e5b3da920b22a19a398b12d12dc61ad
yunomi0087 <newsmile251@outlook.jp>
2020-06-07 19:35:09 +0900

    ○×ゲームjs
    交互に○×が付き、どちらの番かの表示が行われるようになった。

--- gameScreen/ticTacToe.js
+++ gameScreen/ticTacToe.js
@@ -1,11 +1,60 @@
 'use strict';
 
-function() {
-  
+// var cell = document.querySelectorAll('.cell');
+// console.log(cell);
+// if(cell.textContent === ) {
+//   button.disabled = true;
+// }
+
+// function startup() {
+//   var cell = document.querySelectorAll('.cell');
+//   var button = document.getElementById('continue');
+
+//   if(cell.textContent === false) {
+//     button.disabled = true;
+//   }
+// }
+
+// var cellMath = document.querySelectorAll('div.cell').length;
+// document.querySelector('div.game-screen > *').addEventListener('click', () => {
+//   const circlePlayer = "○";
+//   const crossPlayer = "×";
+//   function game(a,b) {
+//     for (var i= 0; i <= 9; i++) {
+//     }
+//   }
+//   const check = document.getElementById('cell1');
+//   const markCircle = check.textContent = "○";
+//   check.appendChild(markCircle);
+// })
+
+var count = 0;
+
+// 初期化
+function initialize() {
+  console.log('initialize');
+}
+// セルクリック
+function clickCell(a) {
+  const p = document.querySelector('.info p');
+  count++
+  if (count%2 === 0) {
+    a.target.textContent = '×';
+    p.textContent = '○の番です';
+  } else {
+    a.target.textContent = '○';
+    p.textContent = '×の番です';
+  }
+}
+// もう一度ボタン押下
+function submitContinueButton() {
+  console.log('submitContinueButton');
 }
 
-document.getElementById('box1').addEventListener('click', () => {
-  const mark = document.createElement('p')
-   
-  if()
+window.onload = initialize;
+var cells = document.querySelectorAll('.cell');
+cells.forEach(function (cell) {
+  cell.addEventListener('click', clickCell);
 });
+document.querySelector('button').onclick = submitContinueButton;
+
