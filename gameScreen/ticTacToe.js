e1d904f61aae31cb6d68534477a6be332ceaab59
yunomi0087 <newsmile251@outlook.jp>
2020-06-11 14:57:09 +0900

    勝利条件
    勝利条件の追加
    ブラウザ上では未実装

--- gameScreen/ticTacToe.js
+++ gameScreen/ticTacToe.js
@@ -31,9 +31,34 @@
 var count = 0;
 var button = document.querySelector('button');
 var p = document.querySelector('.info p');
+var cell1 = document.getElementById('cell1');
+var cell2 = document.getElementById('cell2');
+var cell3 = document.getElementById('cell3');
+var cell4 = document.getElementById('cell4');
+var cell5 = document.getElementById('cell5');
+var cell6 = document.getElementById('cell6');
+var cell7 = document.getElementById('cell7');
+var cell8 = document.getElementById('cell8');
+var cell9 = document.getElementById('cell9');
+var side1 = cell1 && cell2 && cell3;
+var side2 = cell4 && cell5 && cell6;
+var side3 = cell7 && cell8 && cell9;
+var ver1 = cell1 && cell4 && cell7;
+var ver2 = cell2 && cell5 && cell8;
+var ver3 = cell3 && cell6 && cell9;
+var diag1 = cell1 && cell5 && cell9;
+var diag2 = cell3 && cell5 && cell6;
+
+
 // 初期化
 function initialize() {
-  console.log('initialize');
+  cells.forEach(cell => {
+    if(Boolean(cell.textContent) === true ) {
+      cell.textContent = "";
+      count = 0;
+      p.textContent = "○の番です";
+    }
+  });
 }
 // セルクリック
 function clickCell(a) {
@@ -45,7 +70,11 @@ function clickCell(a) {
   if (count%2 === 0) {
     if (check) {
       a.target.textContent = '×';
-      p.textContent = '○の番です';
+      if (side1.textContent === "×") {//修正ポイント
+        p.textContent = '×の勝利です';
+      } else {
+        p.textContent = '○の番です';
+      }
     } 
   } else {
     if (check) {
@@ -56,13 +85,12 @@ function clickCell(a) {
 }
 // もう一度ボタン押下
 function submitContinueButton() {
-  const cells = document.querySelectorAll('.cell');
   cells.forEach(cell => {
     if(Boolean(cell.textContent) === true ) {
       cell.textContent = "";
-      button.disabled = true;
       count = 0;
       p.textContent = "○の番です";
+      button.disabled = true;
     }
   });
 }
