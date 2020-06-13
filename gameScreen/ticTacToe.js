8d93bbae814a4b3dc564dd4087afb5037c11a7f9
yunomi0087 <newsmile251@outlook.jp>
2020-06-13 12:50:53 +0900

    勝利条件
    勝利判定追加。
    勝利後の終了処理の追加作業中

--- gameScreen/ticTacToe.js
+++ gameScreen/ticTacToe.js
@@ -31,24 +31,6 @@
 var count = 0;
 var button = document.querySelector('button');
 var p = document.querySelector('.info p');
-var cell1 = document.getElementById('cell1');
-var cell2 = document.getElementById('cell2');
-var cell3 = document.getElementById('cell3');
-var cell4 = document.getElementById('cell4');
-var cell5 = document.getElementById('cell5');
-var cell6 = document.getElementById('cell6');
-var cell7 = document.getElementById('cell7');
-var cell8 = document.getElementById('cell8');
-var cell9 = document.getElementById('cell9');
-var side1 = cell1 && cell2 && cell3;
-var side2 = cell4 && cell5 && cell6;
-var side3 = cell7 && cell8 && cell9;
-var ver1 = cell1 && cell4 && cell7;
-var ver2 = cell2 && cell5 && cell8;
-var ver3 = cell3 && cell6 && cell9;
-var diag1 = cell1 && cell5 && cell9;
-var diag2 = cell3 && cell5 && cell6;
-
 
 // 初期化
 function initialize() {
@@ -70,17 +52,37 @@ function clickCell(a) {
   if (count%2 === 0) {
     if (check) {
       a.target.textContent = '×';
-      if (side1.textContent === "×") {//修正ポイント
-        p.textContent = '×の勝利です';
+      if (resultjudge[0][0].textContent === '×' && resultjudge[0][1].textContent === '×' && resultjudge[0][2].textContent === '×' 
+      || resultjudge[1][0].textContent === '×' && resultjudge[1][1].textContent === '×' && resultjudge[1][2].textContent === '×' 
+      || resultjudge[2][0].textContent === '×' && resultjudge[2][1].textContent === '×' && resultjudge[2][2].textContent === '×' 
+      || resultjudge[3][0].textContent === '×' && resultjudge[3][1].textContent === '×' && resultjudge[3][2].textContent === '×' 
+      || resultjudge[4][0].textContent === '×' && resultjudge[4][1].textContent === '×' && resultjudge[4][2].textContent === '×' 
+      || resultjudge[5][0].textContent === '×' && resultjudge[5][1].textContent === '×' && resultjudge[5][2].textContent === '×' 
+      || resultjudge[6][0].textContent === '×' && resultjudge[6][1].textContent === '×' && resultjudge[6][2].textContent === '×' 
+      || resultjudge[7][0].textContent === '×' && resultjudge[7][1].textContent === '×' && resultjudge[7][2].textContent === '×') {
+        p.textContent = '×の勝利です！';
+        
       } else {
         p.textContent = '○の番です';
       }
-    } 
+    }
   } else {
     if (check) {
       a.target.textContent = '○';
-      p.textContent = '×の番です';
-    } 
+      if (resultjudge[0][0].textContent === '○' && resultjudge[0][1].textContent === '○' && resultjudge[0][2].textContent === '○' 
+      || resultjudge[1][0].textContent === '○' && resultjudge[1][1].textContent === '○' && resultjudge[1][2].textContent === '○' 
+      || resultjudge[2][0].textContent === '○' && resultjudge[2][1].textContent === '○' && resultjudge[2][2].textContent === '○' 
+      || resultjudge[3][0].textContent === '○' && resultjudge[3][1].textContent === '○' && resultjudge[3][2].textContent === '○' 
+      || resultjudge[4][0].textContent === '○' && resultjudge[4][1].textContent === '○' && resultjudge[4][2].textContent === '○' 
+      || resultjudge[5][0].textContent === '○' && resultjudge[5][1].textContent === '○' && resultjudge[5][2].textContent === '○' 
+      || resultjudge[6][0].textContent === '○' && resultjudge[6][1].textContent === '○' && resultjudge[6][2].textContent === '○' 
+      || resultjudge[7][0].textContent === '○' && resultjudge[7][1].textContent === '○' && resultjudge[7][2].textContent === '○') {
+        p.textContent ='○の勝利です！';
+        
+      } else {
+        p.textContent = '×の番です';
+      }
+    }
   }
 }
 // もう一度ボタン押下
@@ -101,3 +103,21 @@ cells.forEach(function (cell) {
   cell.addEventListener('click', clickCell);
 });
 document.querySelector('button').onclick = submitContinueButton;
+
+var cellsArray = [].slice.call(cells);
+function filterById(targetArray,Id) {
+  return targetArray.filter(function(e) {
+    return (e.id === Id[0] || e.id === Id[1] ||  e.id === Id[2]);
+  });
+}
+
+var side1 = filterById(cellsArray, ['cell1', 'cell2', 'cell3']);
+var side2 = filterById(cellsArray, ['cell4', 'cell5', 'cell6']);
+var side3 = filterById(cellsArray, ['cell7', 'cell8', 'cell9']);
+var ver1 = filterById(cellsArray, ['cell1', 'cell4', 'cell7']);
+var ver2 = filterById(cellsArray, ['cell2', 'cell5', 'cell8']);
+var ver3 = filterById(cellsArray, ['cell3', 'cell6', 'cell9']);
+var diag1 = filterById(cellsArray, ['cell1', 'cell5', 'cell9']);
+var diag2 = filterById(cellsArray, ['cell3', 'cell5', 'cell7']);
+
+var resultjudge = [side1, side2, side3, ver1, ver2, ver3, diag1, diag2];
